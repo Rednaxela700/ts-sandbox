288fde3dab188239ae4455cd598ecfc2af9e0559
rednaxela700 <ohleg@windowslive.com>
2020-10-13 19:19:45 +0200

    types.ts: added typed arrays containing primitives

--- types.js
+++ types.js
@@ -1,7 +1,14 @@
 var myString;
 var myNum;
 var myBool;
+//this has to be an array of strings
+var strArray;
+var numArr;
+var boolArr;
 myNum = 22.555;
 myString = 'hello';
 myBool = false;
+strArray = ['hello', 'stranger'];
+numArr = [1, 2, 3];
+boolArr[true, true, false];
 console.log(myBool);
--- types.ts
+++ types.ts
@@ -2,9 +2,16 @@ let myString: string;
 let myNum: number;
 let myBool: boolean
 
+//this has to be an array of strings
+let strArray: string[]
+let numArr: number[]
+let boolArr: boolean[]
 
 myNum = 22.555;
 myString = 'hello';
 myBool = false;
+strArray = ['hello', 'stranger']
+numArr = [1,2,3]
+boolArr [true, true, false]
 
 console.log(myBool)
